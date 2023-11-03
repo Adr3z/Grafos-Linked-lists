102e3cdb940e007ffcdc5d014d311011357d00f3
Adreeezz <127537483+Adreeezz@users.noreply.github.com>
2023-11-03 00:01:58 +0000

    Recorrido bfs y creo que ya arreglé la memoria :D

--- graph.c
+++ graph.c
@@ -5,20 +5,22 @@
 Graph* createGraph(graph_type_t type) 
 {
     Graph* graph = (Graph*)malloc(sizeof(Graph));
-    graph->num_vertices = 0; 
-    graph->graph_type = type;
-    graph->vertex_list = NULL;
-    graph->adjacency_matrix = NULL;
+    if (graph != NULL) {
+        graph->num_vertices = 0; 
+        graph->graph_type = type;
+        graph->vertex_list = NULL;
+        graph->adjacency_matrix = NULL;
+    }
     return graph;
 }
 
 int **adj_mat(int num_vertices)
 {
-    int **mat = (int**)malloc(num_vertices * sizeof(int*));
-    for(int i = 0; i < num_vertices; i++){
-        *(mat + i) = (int*)malloc(num_vertices * sizeof(int));
-        for(int j = 0; j < num_vertices; j++){
-            *(*(mat +i) + j) = 0;
+    int **mat = (int **)malloc(num_vertices * sizeof(int *));
+    for (int i = 0; i < num_vertices; i++) {
+        *(mat + i) = (int *)malloc(num_vertices * sizeof(int));
+        for (int j = 0; j < num_vertices; j++) {
+            *(*(mat + i) + j) = 0;
         }
     }
     return mat;
@@ -53,6 +55,7 @@ void destroyGraph(Graph* graph)
         for (int i = 0; i < graph->num_vertices; i++) {
             free(*(graph->adjacency_matrix + i));
         }
+        free(graph->adjacency_matrix);
     }
 
     // Liberar los vértices y las aristas
@@ -210,6 +213,103 @@ void removeEdgeFromVertex(Vertex *vertex, Vertex *neighbor)
         prev_edge = current_edge;
         current_edge = current_edge->next;
     }
+}
+
+
+void BFS(Graph *graph, char start_vertex_id) 
+{
+    // Encuentra el vértice de inicio
+    Vertex *start_vertex = findVertexByChar(graph, start_vertex_id);
+    if (start_vertex == NULL) {
+        printf("El vértice de inicio no se encontró en el grafo.\n");
+        return;
+    }
+
+    // Inicializa una cola para el recorrido BFS
+    Queue *queue = createQueue();
+
+    // Marca todos los vértices como no visitados
+    Vertex *current_vertex = graph->vertex_list;
+    while (current_vertex != NULL) {
+        current_vertex->visited = 0;
+        current_vertex = current_vertex->next;
+    }
+
+    // Marca el vértice de inicio como visitado y encola
+    start_vertex->visited = 1;
+    enqueue(queue, start_vertex);
+
+    while (isEmpty(queue) == 0) {
+        // Desencola un vértice
+        Vertex *current_vertex = dequeue(queue);
+        printf("%c ", current_vertex->id);
+
+        // Procesa los vértices adyacentes no visitados
+        Edge *edge = current_vertex->edge_list;
+        while (edge != NULL) {
+            Vertex *neighbor = edge->vertex;
+            if (!neighbor->visited) {
+                neighbor->visited = 1;
+                enqueue(queue, neighbor);
+            }
+            edge = edge->next;
+        }
+    }
+
+    printf("}\n");
+
+    // Libera la cola
+    free(queue);
+}
+
+Vertex* dequeue(Queue *queue) 
+{
+    if (isEmpty(queue)) {
+        return NULL;
+    }
+
+    QueueNode *temp = queue->front;
+    Vertex *vertex = temp->vertex;
+
+    if (queue->front == queue->rear) {
+        queue->front = NULL;
+        queue->rear = NULL;
+    } else {
+        queue->front = queue->front->next;
+    }
+
+    free(temp);
+    return vertex;
+}
+
+void enqueue(Queue *queue, Vertex *vertex) 
+{
+    QueueNode *newNode = (QueueNode*)malloc(sizeof(QueueNode));
+    newNode->vertex = vertex;
+    newNode->next = NULL;
+
+    if (queue->rear == NULL) {
+        queue->front = newNode;
+        queue->rear = newNode;
+    } else {
+        queue->rear->next = newNode;
+        queue->rear = newNode;
+    }
+}
+
+int isEmpty(Queue *queue) {
+    if(queue->front == NULL){
+        return 1;
+    }
+    return 0;
+}
+
+Queue* createQueue() 
+{
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    queue->front = NULL;
+    queue->rear = NULL;
+    return queue;
 }
 
 //main functions
